a181c5de99cdccf4e6932d1ab9835a983dd2462a
Jinxuan Cang <jinxuancang@gmail.com>
2019-02-04 15:41:00 -0500

    Commenting masterAJAX.js

--- syossetIntra/WebContent/js/masterAJAX.js
+++ syossetIntra/WebContent/js/masterAJAX.js
@@ -1,32 +1,45 @@
 function ajax(settings) {
-	//timing function
-	var time = new Date;
-	var sendtime = time.getTime();
+	//declare timing variable
+	var timestamp = new Date;
+	// define ajax object
 	var ajax = new XMLHttpRequest();
+	// define and get time
+	var requestTime = timestamp.getTime();
+	var responseTime;
+	var reqDuration;
+
+	//ajax object open connection
 	ajax.open(settings.method, settings.path, true);
-    if (settings.method=="POST") {
-        ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        ajax.send(settings.input);
-    }
-    else ajax.send();
-    ajax.onreadystatechange = function() {
-	if (ajax.readyState==4) {
-		if (ajax.status == 200) {
-			var time = new Date;
-            		var inter_time = time.getTime()-sendtime;
-            		GLB_AJAX_Interval = ms_s(inter_time);//undefined function
-            		console.log("AJAX finished in "+GLB_AJAX_Interval)
-           		console.log("%c"+"AJAX Success.","color: green;");
-           		settings.success(ajax.responseText);
-		}
-		else {
-			if (ajax.status == 0) {
-				//loadMessage("error","No Server connection. System unable to operate normally.", true);
+
+	// if POST, set special request header, send contents
+	// else send nothing
+	if (settings.method=="POST") {
+		ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		ajax.send(settings.input);
+	}
+	else
+		ajax.send();
+
+	//detect ajax call state change
+	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4) {
+			if (ajax.status == 200) {
+				timestamp = new Date;
+				responseTime = (new Date).getTime();
+				reqDuration = timestamp.getTime()-requestTime;
+				GLB_AJAX_Interval = ms_s(inter_time);//undefined function
+				console.log("AJAX finished in "+GLB_AJAX_Interval)
+				console.log("%c"+"AJAX Success.","color: green;");
+				settings.success(ajax.responseText);
 			}
 			else {
-				//loadMessage("error","XML Http Request failed. Return code: <b>"+ajax.status+" "+ajax.statusText+"</b>.", true);
+				if (ajax.status == 0) {
+					//loadMessage("error","No Server connection. System unable to operate normally.", true);
+				}
+				else {
+					//loadMessage("error","XML Http Request failed. Return code: <b>"+ajax.status+" "+ajax.statusText+"</b>.", true);
+				}
 			}
 		}
 	}
-    }
 }
