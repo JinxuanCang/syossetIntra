4e677fa0cbb6481dcf223e8cd1792a4a1677fc96
shs-dev0 <shs-dev0@shsdev0-HP-ProDesk-400-G1-SFF>
2019-02-15 16:05:17 -0500

    Started Department Class

---
+++ src/models/Department.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Department {
+	
+	String name;
+	String description;
+	ArrayList<Coordinator> coordinators;
+	ArrayList<Course> courses;
+	//Image sequence;?
+	boolean isSpecialProgram;
+}
