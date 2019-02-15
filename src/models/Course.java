903f6fe7b816b4c8017d89d42902458707c9da9c
shs-dev0 <shs-dev0@shsdev0-HP-ProDesk-400-G1-SFF>
2019-02-15 16:05:27 -0500

    Started Course Class

---
+++ src/models/Course.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Course {
+	int id;
+	int college_id;
+	String name;
+	//CourseType type
+	int [] grades;
+	int year_length;
+	boolean isAlternating;
+	int credits; // don't use floating-point arithmetic, possibly represent half-credits?
+	int college_credits;
+	int period_length;
+	
+	// can use course id arrays as references if slow or too much memory
+	ArrayList<Course> prerequisites;
+	ArrayList<Course> recommended_prerequisites;
+	ArrayList<Course> required_for;
+	ArrayList<Course> recommeded_for;
+	
+	String course_link; // can be URL possibly or link enabled in HTML
+	//expenses?
+	String description;
+	String extra_info;
+}
