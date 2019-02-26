6db2a36f438047a394f112909234d68d03133a70
shs-dev0 <shs-dev0@shsdev0-HP-ProDesk-400-G1-SFF>
2019-02-26 17:02:30 -0500

    added requirements, curriculum, introduction

---
+++ src/models/ExaminationRequirement.java
@@ -0,0 +1,9 @@
+package models;
+
+import java.util.ArrayList;
+
+public class ExaminationRequirement {
+	String name;
+	ArrayList<Course> courses;
+	int minimumTestGrade;
+}
---
+++ src/models/GraduationRequirement.java
@@ -0,0 +1,6 @@
+package models;
+
+public class GraduationRequirement {
+	Department department;
+	int credits; //tenfold
+}
