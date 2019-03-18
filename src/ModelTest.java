f73b0d5917893dee4814efe4f7d13a2f1de4b47e
shs-dev0 <shs-dev0@shsdev0-HP-ProDesk-400-G1-SFF>
2019-03-18 16:57:52 -0400

    Create new test program for importing the curriculum models

---
+++ src/ModelTest.java
@@ -0,0 +1,61 @@
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import models.Curriculum;
+
+@WebServlet("/ModelTest")
+public class ModelTest extends HttpServlet implements Settings {
+	private static final long serialVersionUID = 1L;
+	// create codec registries for POJOs
+	CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+			fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+	// registers POJO object classes
+	// DB access authentication
+	static com.mongodb.client.MongoClient mongo; // bind port
+	{
+		MongoClientSettings settings = MongoClientSettings.builder().codecRegistry(pojoCodecRegistry).build();
+		mongo = MongoClients.create(settings);
+	}
+
+	// must, vital database access info storage
+	MongoDatabase database = mongo.getDatabase("DemoSite");
+	MongoCollection<Curriculum> collection = database.getCollection("ModelTest", Curriculum.class);
+
+	FindIterable<Document> documents;
+
+	// constructor
+	public ModelTest() {
+		super();
+	}
+
+	private void test() {
+		Curriculum curriculum = new Curriculum();
+		curriculum.setYear(2018);
+		//curriculum.set
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		test();
+		response.getWriter().append("Check Console");
+	}
+}
