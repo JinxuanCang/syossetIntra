608b5007f5f2236907f82cc5e39562f0e0859d82
shs-dev0 <shs-dev0@shsdev0-HP-ProDesk-400-G1-SFF>
2019-03-27 17:49:44 -0400

    Startup user collection

---
+++ src/models/user/User.java
@@ -0,0 +1,15 @@
+package models.user;
+
+public class User {
+	private int id;
+	
+	private String firstName;
+	private String lastName;
+	private String middleInitial;
+	
+	private String prefix;
+	private String title;
+	
+	private String phoneNumber;
+	private String email;
+}
